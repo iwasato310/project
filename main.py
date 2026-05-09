4fcc63287064992c37f45c1d41a223f75964aa55
iwasato310 <iwa_sato@hotmail.com>
2026-05-09 16:54:39 +0900

    add dynamic FastAPI endpoint test

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello Docker FastAPI"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello {name}"}
