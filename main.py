11e4bd5eb813806a70a319c5b80bd088f93124dd
iwasato310 <iwa_sato@hotmail.com>
2026-05-13 23:34:09 +0900

    add POST API integration test

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
 
 app = FastAPI()
 
@@ -10,3 +14,7 @@ def read_root():
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/echo")
+def echo_user(user: User):
+    return {"message": f"Hello: {user.name}"}
